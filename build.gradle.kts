c84750dfff6dbfd7ee5462bbfdd74b939dd432c4
Denis Zhdanov <denis.zhdanov@dkatalis.com>
2024-04-28 14:26:52 +0800

    harmonysoft-libs 3.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-val harmonysoftLibsVersion by extra { "3.0.0" }
+val harmonysoftLibsVersion by extra { "3.1.0" }
 
 subprojects {
     extra["projectType"] = "library"
@@ -7,7 +7,6 @@ subprojects {
     version = "2.0.0"
 
     dependencies {
-        api("tech.harmonysoft:inpertio-client-kotlin-spring:1.5.0")
         api("tech.harmonysoft:harmonysoft-slf4j-spring:$harmonysoftLibsVersion")
         api("tech.harmonysoft:harmonysoft-default-implementations:$harmonysoftLibsVersion")
     }
