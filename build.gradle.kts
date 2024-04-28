1fc631b8f045567e341700e60ba6618a4de5e04f
Denis Zhdanov <denis.zhdanov@dkatalis.com>
2024-04-28 13:56:47 +0800

    apply the latest harmonysoft libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
-val harmonysoftLibsVersion by extra { "1.103.0" }
+val harmonysoftLibsVersion by extra { "3.0.0" }
 
 subprojects {
     extra["projectType"] = "library"
     extra["projectUrl"] = "https://github.com/mental-mate/libraries.git"
 
-    version = "1.9.0"
+    version = "2.0.0"
 
     dependencies {
         api("tech.harmonysoft:inpertio-client-kotlin-spring:1.5.0")
